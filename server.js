57b42fec407242533bcf6468ed91a397ddf38d68
Valtteri Ranta <waltherbeach@gmail.com>
2021-01-19 09:41:36 +0200

    MongoDB connected

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
+const db = process.env.DB_CONN;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 const PORT = process.env.port || 5000;
 
