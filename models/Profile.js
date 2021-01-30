7f5cde4d1129aad34a1779cc8c7dc7f5e07e1996
Valtteri Ranta <waltherbeach@gmail.com>
2021-01-30 09:54:33 +0200

    Schema for profile, not yet tested

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,41 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
-
+  name: {
+    type: String,
+    required: [true, 'Please add name'],
+  },
+  address: {
+    street: {
+      type: String,
+      required: [true, 'Please add street address'],
+    },
+    zipcode: {
+      type: String,
+      required: [true, 'Please add zipcode'],
+    },
+    city: {
+      type: String,
+      required: [true, 'Please add city'],
+    },
+  },
+  email: {
+    type: String,
+    match: [
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+      'Please add a valid email',
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [16, 'Max length for phone number is 16 characters'],
+  },
+  social: {
+    linkedin: String,
+    github: String,
+    scholar: String,
+  },
+  photo: String,
 });
 
 module.exports = mongoose.model('profile', ProfileSchema);
