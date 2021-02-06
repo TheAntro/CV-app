093b87ed95166328becbd7f0ce83dbab18d1b784
Valtteri Ranta <waltherbeach@gmail.com>
2021-02-06 11:27:08 +0200

    Added UserSchema, for authentication.

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    match: [
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+      'Please add a valid email',
+    ],
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'user',
+  },
+});
+
+module.exports = mongoose.model('user', UserSchema);
